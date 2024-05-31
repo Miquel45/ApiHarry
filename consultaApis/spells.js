bb921004f80b30e0b15a46453ec5f54b97c29522
miquelpomar <miguelpomar@alumnat.iesdamiahuguet.net>
2024-05-31 08:53:27 +0200

    finalitzat

---
+++ consultaApis/spells.js
@@ -0,0 +1,32 @@
+async function getspells() {
+    const url='https://potterapi-fedeperin.vercel.app/es/spells'
+        document.getElementById('button').hidden=true
+        fetch(url)
+            .then(response=> response.json() )
+            .then(data => {
+                const table = document.getElementById("info");
+
+                data.forEach((e,i) => {    //< ---  recorremos data
+
+                    let tr = document.createElement("tr"); //< ---  creamos una fila
+
+                    let td = document.createElement("td"); //< ---  Hacemos columna index dentro de la fila
+                    td.classList.add("index");
+                    td.innerHTML = i;
+                    tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                    for (p in e) {  //< ---  recorremos cada propiedad de cada elemento
+
+                        let td = document.createElement("td"); //< ---  Hacemos columna dentro de la fila
+                        td.classList.add(p);//<-- le podemos agregar a toda la columna la misma clase
+                        td.innerHTML = e[p];
+
+                        tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                    }
+
+                    table.appendChild(tr); //< --- Agregamos la fila a la tabla
+                });
+            })
+            .catch(err=>console.log(err))
+    }
